d31a59dabc32b17b60b9a78d8fb6de5bb95d7360
ShkulovNikita <tyuiop_2@mail.ru>
2022-08-29 14:43:32 +0700

    Добавлен класс для хранения данных из файла настроек.

---
+++ Task1/Task1/Configurator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    /// <summary>
+    /// Класс для работы с файлом настроек
+    /// </summary>
+    static public class Configurator
+    {
+
+    }
+}
---
+++ Task1/Task1/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    /// <summary>
+    /// Класс с данными из файла настроек
+    /// </summary>
+    class Settings
+    {
+        public Settings()
+        {
+            OriginalPaths = new List<string>();
+        }
+
+        /// <summary>
+        /// Список путей до исходных папок
+        /// </summary>
+        public List<string> OriginalPaths { get; set; }
+
+        /// <summary>
+        /// Путь до папки назначения
+        /// </summary>
+        public string DestinationPath { get; set; }
+    }
+}
