c41df60a50bf0921602adfc259e04b2c5459a4fe
ShkulovNikita <tyuiop_2@mail.ru>
2022-08-31 18:08:42 +0700

    Добавлена возможность чтения файла настроек.

---
+++ Task2/Task2/Helpers/Configurator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Task2.Models;
+
+namespace Task2.Helpers
+{
+    /// <summary>
+    /// Класс для работы с файлом настроек
+    /// </summary>
+    static public class Configurator
+    {
+        /// <summary>
+        /// Получить настройки из файла
+        /// </summary>
+        /// <returns>Настройки в виде объекта</returns>
+        static public Config GetSettings()
+        {
+            Config settings;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Config));
+            try
+            {
+                using (FileStream fs = new FileStream("config.xml", FileMode.OpenOrCreate))
+                {
+                    settings = xmlSerializer.Deserialize(fs) as Config;
+                }
+
+                return settings;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Task2/Task2/Models/Config.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Task2.Models
+{
+    /// <summary>
+    /// Класс для использования данных, получаемых из файла настроек, в виде объекта
+    /// </summary>
+    public class Config
+    {
+        public Config() { }
+
+        /// <summary>
+        /// Добавленные RSS-ленты
+        /// </summary>
+        public List<string> Feeds { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Частота обновления в миллисекундах
+        /// </summary>
+        public int Update { get; set; }
+
+        /// <summary>
+        /// Адрес прокси-сервера
+        /// </summary>
+        public string Proxy { get; set; }
+
+        /// <summary>
+        /// Логин для авторизации в прокси-сервере
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль для авторизации в прокси-сервере
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
